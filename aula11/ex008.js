36d7b9e4696a3dbcab42d31832426b3e4d381512
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2022-04-27 19:12:15 +0100

    Added new codes

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60;
+if (vel <= 60) {
+  console.log(`A velocidade do carro é ${vel}km/h`);
+  console.log("Dirija sempre usando cinto de segurança");
+}
+if (vel > 60) {
+  console.log("Você ulttrapassou a velocidade permitida. MULTADO!");
+}
